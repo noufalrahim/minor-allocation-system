0b95863fc6f865d7ec8a77d498909a94457aa357
noufalrahim <noufalrahim6784@gmail.com>
2024-10-08 11:39:49 +0530

    style: Removed excess padding from verification box

--- components/DetailsCard/index.tsx
+++ components/DetailsCard/index.tsx
@@ -43,45 +43,43 @@ export default function DetailsCard(
             { label: "SGPA 1", value: studentData.sgpaS1 },
             { label: "SGPA 2", value: studentData.sgpaS2 },
         ])
-        
+
     }, [studentData]);
 
-    if(studentData === null) {
+    if (studentData === null) {
         return;
     }
 
     return (
-        <div className="dark:bg-[#1F2937] bg-[#D4DDFF] h-full lg:h-80 xl:h-80 2xl:h-80 3xl:h-80 px-20 flex flex-col rounded-lg text-white shadow-lg w-11/12 lg:w-[60rem] xl:w-[60rem] 2xl:w-[60rem] 3xl:w-[60rem]">
-            <>
-                <div className="flex justify-between items-start h-full mt-5 flex-col lg:flex-row xl:flex-row 2xl:flex-row 3xl:flex-row">
-                    <div className="justify-around items-start flex h-full flex-col">
-                        {
-                            fields.slice(0, 3).map((field, index) => (
-                                <Fields key={index} label={field.label} value={field.value} />
-                            ))
-                        }
-                    </div>
-                    <div className="justify-around items-start flex h-full flex-col">
-                        {
-                            fields.slice(3, 6).map((field, index) => (
-                                <Fields key={index} label={field.label} value={field.value} />
-                            ))
-                        }
-                    </div>
-                    <div className="justify-around items-start flex h-full flex-col">
-                        {
-                            fields.slice(6, 9).map((field, index) => (
-                                <Fields key={index} label={field.label} value={field.value} />
-                            ))
-                        }
-                    </div>
+        <div className="dark:bg-[#1F2937] bg-[#D4DDFF] h-full lg:h-80 xl:h-80 2xl:h-80 3xl:h-80 px-5 flex flex-col rounded-lg text-white shadow-lg w-11/12 lg:w-[60rem] xl:w-[60rem] 2xl:w-[60rem] 3xl:w-[60rem]">
+            <div className="flex justify-between w-full items-start h-full mt-5 flex-col lg:flex-row xl:flex-row 2xl:flex-row 3xl:flex-row">
+                <div className="justify-around items-start flex h-full flex-col">
+                    {
+                        fields.slice(0, 3).map((field, index) => (
+                            <Fields key={index} label={field.label} value={field.value} />
+                        ))
+                    }
                 </div>
-                <div className="w-full items-center justify-center mt-5 flex">
-                    <button disabled={verified} className={`${verified ? "bg-[#8e9092c8]" : "bg-[#4E7396]"}` + " px-5 py-2 mb-5 w-24 text-center flex items-center justify-center rounded-md"} onClick={handleConfirm}>
-                        {verified ? "Confirmed" : "Confirm"}
-                    </button>
+                <div className="justify-around items-start flex h-full flex-col">
+                    {
+                        fields.slice(3, 6).map((field, index) => (
+                            <Fields key={index} label={field.label} value={field.value} />
+                        ))
+                    }
                 </div>
-            </>
+                <div className="justify-around items-start flex h-full flex-col">
+                    {
+                        fields.slice(6, 9).map((field, index) => (
+                            <Fields key={index} label={field.label} value={field.value} />
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="w-full items-center justify-center mt-5 flex">
+                <button disabled={verified} className={`${verified ? "bg-[#8e9092c8]" : "bg-[#4E7396]"}` + " px-5 py-2 mb-5 w-24 text-center flex items-center justify-center rounded-md"} onClick={handleConfirm}>
+                    {verified ? "Confirmed" : "Confirm"}
+                </button>
+            </div>
         </div>
     )
 }
